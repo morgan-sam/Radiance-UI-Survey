2d771898314ec50c101a0982ac6964a9f60c115a
Sam <samuel_morgan_@outlook.com>
2020-09-10 12:30:14 +0100

    splash-text-container

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,13 @@ import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { Button } from "radiance-ui"
+import { LinkButton, Button } from "radiance-ui"
 import { PlanIcon } from "radiance-ui/lib/icons"
 import { Callout, Typography } from "radiance-ui"
 import "../css/index.css"
 import PatternAttacher from "../components/patternAttacher"
 import blob from "../svg/blob.svg"
+import COLORS from "tinycolor2"
 
 const imgURL =
   "https://images.unsplash.com/photo-1570554520913-ce2192a74574?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
@@ -17,11 +18,24 @@ const IndexPage = () => (
     <SEO title="Home" />
     <div className="splash">
       <img className="splash-image"></img>
+      <div className="splash-text-container">
+        <div className="splash-main-text">Need a check up?</div>
+        <div className="splash-sub-text">
+          Take a quick survey to see if we can help.
+        </div>
+        <LinkButton
+          buttonColor={COLORS.statusRed}
+          buttonType="primary"
+          style={{ minWidth: "4rem", userSelect: "none" }}
+        >
+          TAKE SURVEY
+        </LinkButton>
+      </div>
     </div>
-    {/* <div className="explanation-box">
+    {/* {/* <div className="explanation-box">
       <PatternAttacher src={imgURL} x={"3rem"} y={"3rem"} />
-    </div>
-    <div className="survey-box">
+    </div> */}
+    {/* <div className="survey-box">
       <Typography.Display>Display</Typography.Display>
       <Callout.Container>
         <Callout icon={<PlanIcon width={32} height={32} />}>

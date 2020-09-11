cc90db8a9bbd96e43625c039447f2d506a69f50c
Sam <samuel_morgan_@outlook.com>
2020-09-11 19:19:30 +0100

    IndexPage not inline fn

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,92 +26,17 @@ const thirdImage =
 const fourthImage =
   "https://images.unsplash.com/photo-1556228578-626e9590b81f?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <div className="section splash">
-      <img className="splash-image"></img>
-      <div className="splash-text-container">
-        <div className="splash-main-text">Need a check up?</div>
-        <div className="splash-sub-text">
-          Take a quick survey to see if we can help.
-        </div>
-        <div className="survey-container">
-          <Button
-            buttonColor={COLORS.statusRed}
-            buttonType="primary"
-            style={{ minWidth: "4rem", userSelect: "none" }}
-          >
-            TAKE SURVEY
-          </Button>
-          <a className="survey-privacy-text">Privacy Policy</a>
-        </div>
-      </div>
-    </div>
-    <div className="section testimonies">
-      <div className="testimony-container">
-        <div className="testimony-title">{lorem(7)}</div>
-        <div className="testimony-paragraph">{lorem(36)}</div>
-      </div>
-      <div className="testimony-title">{lorem(5)}</div>
-      <PatternAttacher
-        src={firstImage}
-        svg={blob}
-        x={"-3rem"}
-        y={"-3rem"}
-        height={"30rem"}
-        color={"hsl(80,80%,70%)"}
-      />
-      <div className="testimony-container">
-        <div className="testimony-title">{lorem(7)}</div>
-        <div className="testimony-paragraph">{lorem(36)}</div>
-      </div>
-      <div className="testimony-title">{lorem(5)}</div>
-      <PatternAttacher
-        src={secondImage}
-        svg={star}
-        x={"3rem"}
-        y={"-3rem"}
-        height={"30rem"}
-        color={"hsl(47,70%,65%)"}
-      />
-    </div>
-    <div className="section pocket-guy">
-      <div className="pre-gradient"></div>
-      <div className="white-overlay-text">{lorem(7)}</div>
-      <img src={thirdImage} className="fill-image" />
-    </div>
-    <div className="section callouts">
-      <div className="callout-container">
-        <Typography.Display>{lorem(2)}</Typography.Display>
-        <Callout.Container>
-          <Callout icon={<PlanIcon width={32} height={32} />}>
-            <div className="callout-title">{lorem(4)}</div>
-            <div className="callout-paragraph">{lorem(12)}</div>
-          </Callout>
-        </Callout.Container>
-        <Callout.Container>
-          <Callout icon={<GiftIcon width={32} height={32} />}>
-            <div className="callout-title">{lorem(4)}</div>
-            <div className="callout-paragraph">{lorem(12)}</div>
-          </Callout>
-        </Callout.Container>
-        <Callout.Container>
-          <Callout icon={<SmileIcon width={32} height={32} />}>
-            <div className="callout-title">{lorem(4)}</div>
-            <div className="callout-paragraph">{lorem(12)}</div>
-          </Callout>
-        </Callout.Container>
-      </div>
-    </div>
-    <div className="section toast">
-      <div className="pre-gradient"></div>
-      <img src={fourthImage} />
-      <div className="white-overlay-text">{lorem(7)}</div>
-      <div className="toast-after">
-        <div className="toast-text-container">
-          <div className="toast-text-title">{lorem(4)}</div>
-          <List items={new Array(4).fill(lorem(6))} />
+const IndexPage = () => {
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <div className="section splash">
+        <img className="splash-image"></img>
+        <div className="splash-text-container">
+          <div className="splash-main-text">Need a check up?</div>
+          <div className="splash-sub-text">
+            Take a quick survey to see if we can help.
+          </div>
           <div className="survey-container">
             <Button
               buttonColor={COLORS.statusRed}
@@ -124,8 +49,85 @@ const IndexPage = () => (
           </div>
         </div>
       </div>
-    </div>
-  </Layout>
-)
+      <div className="section testimonies">
+        <div className="testimony-container">
+          <div className="testimony-title">{lorem(7)}</div>
+          <div className="testimony-paragraph">{lorem(36)}</div>
+        </div>
+        <div className="testimony-title">{lorem(5)}</div>
+        <PatternAttacher
+          src={firstImage}
+          svg={blob}
+          x={"-3rem"}
+          y={"-3rem"}
+          height={"30rem"}
+          color={"hsl(80,80%,70%)"}
+        />
+        <div className="testimony-container">
+          <div className="testimony-title">{lorem(7)}</div>
+          <div className="testimony-paragraph">{lorem(36)}</div>
+        </div>
+        <div className="testimony-title">{lorem(5)}</div>
+        <PatternAttacher
+          src={secondImage}
+          svg={star}
+          x={"3rem"}
+          y={"-3rem"}
+          height={"30rem"}
+          color={"hsl(47,70%,65%)"}
+        />
+      </div>
+      <div className="section pocket-guy">
+        <div className="pre-gradient"></div>
+        <div className="white-overlay-text">{lorem(7)}</div>
+        <img src={thirdImage} className="fill-image" />
+      </div>
+      <div className="section callouts">
+        <div className="callout-container">
+          <Typography.Display>{lorem(2)}</Typography.Display>
+          <Callout.Container>
+            <Callout icon={<PlanIcon width={32} height={32} />}>
+              <div className="callout-title">{lorem(4)}</div>
+              <div className="callout-paragraph">{lorem(12)}</div>
+            </Callout>
+          </Callout.Container>
+          <Callout.Container>
+            <Callout icon={<GiftIcon width={32} height={32} />}>
+              <div className="callout-title">{lorem(4)}</div>
+              <div className="callout-paragraph">{lorem(12)}</div>
+            </Callout>
+          </Callout.Container>
+          <Callout.Container>
+            <Callout icon={<SmileIcon width={32} height={32} />}>
+              <div className="callout-title">{lorem(4)}</div>
+              <div className="callout-paragraph">{lorem(12)}</div>
+            </Callout>
+          </Callout.Container>
+        </div>
+      </div>
+      <div className="section toast">
+        <div className="pre-gradient"></div>
+        <img src={fourthImage} />
+        <div className="white-overlay-text">{lorem(7)}</div>
+        <div className="toast-after">
+          <div className="toast-text-container">
+            <div className="toast-text-title">{lorem(4)}</div>
+            <List items={new Array(4).fill(lorem(6))} />
+            <div className="survey-container">
+              <Button
+                buttonColor={COLORS.statusRed}
+                buttonType="primary"
+                style={{ minWidth: "4rem", userSelect: "none" }}
+              >
+                TAKE SURVEY
+              </Button>
+              <a className="survey-privacy-text">Privacy Policy</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage

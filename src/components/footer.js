3473b487332e53fb4fdc4702cb72542b09d55ad7
Sam <samuel_morgan_@outlook.com>
2020-09-11 17:59:36 +0100

    Footer links flex grid

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,6 +6,22 @@ const Footer = ({ siteTitle }) => {
   return (
     <div className="footer">
       <h1 className="logo">{siteTitle}</h1>
+      <div className="footer-links-horizontal">
+        <div className="footer-link-container">
+          <a className="footer-link">Why Curology</a>
+          <a className="footer-link">Community</a>
+          <a className="footer-link">Out Story</a>
+          <a className="footer-link">Guides</a>
+          <a className="footer-link">Survey</a>
+        </div>
+        <div className="footer-link-container">
+          <a className="footer-link">Blog</a>
+          <a className="footer-link">Support</a>
+          <a className="footer-link">Reviews</a>
+          <a className="footer-link">Careers</a>
+          <a className="footer-link">Contact Us</a>
+        </div>
+      </div>
       <div>
         © {new Date().getFullYear()} {siteTitle}
       </div>

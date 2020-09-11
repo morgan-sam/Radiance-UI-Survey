7f81f7c41216e964768ea6f8f3553a762ec5016c
Sam <samuel_morgan_@outlook.com>
2020-09-11 19:07:35 +0100

    Created social media section

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,12 @@
 import React from "react"
 
+import instagram from "../svg/social_media/instagram.svg"
+import facebook from "../svg/social_media/facebook.svg"
+import snapchat from "../svg/social_media/snapchat.svg"
+import twitter from "../svg/social_media/twitter.svg"
+import youtube from "../svg/social_media/youtube.svg"
+import pinterest from "../svg/social_media/pinterest.svg"
+
 import "../css/footer.css"
 
 const Footer = ({ siteTitle }) => {
@@ -22,6 +29,20 @@ const Footer = ({ siteTitle }) => {
             <a className="footer-link">Careers</a>
             <a className="footer-link">Contact Us</a>
           </div>
+        </div>
+      </div>
+      <div className="footer-section">
+        <div className="social-media-container">
+          <div>Follow @curology</div>
+          <div className="icon-container">
+            <img src={instagram} />
+            <img src={facebook} />
+            <img src={snapchat} />
+            <img src={twitter} />
+            <img src={youtube} />
+            <img src={pinterest} />
+          </div>
+          <a>Need financial assistance?</a>
         </div>
       </div>
       <div className="footer-section">

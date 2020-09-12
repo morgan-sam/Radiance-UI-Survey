4b43b025916b1baa8a4823c88a149bf805ccb36c
Sam <samuel_morgan_@outlook.com>
2020-09-12 10:25:07 +0100

    Fixed RadioButton margin

--- src/components/survey.js
+++ src/components/survey.js
@@ -1,6 +1,7 @@
 import React from "react"
-import { ImmersiveModal, Button, OptionButton } from "radiance-ui"
+import { ImmersiveModal, Button, OptionButton, RadioButton } from "radiance-ui"
 import { lorem } from "../js/lorem"
+import { css } from "@emotion/core"
 
 const modalHeaderImage =
   "https://images.unsplash.com/photo-1556228852-80b6e5eeff06?ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80"
@@ -22,21 +23,29 @@ const Survey = props => {
         Thank you for taking our survey. It will take approximately 2-3 minutes.
       </p>
       <p>Question 1:</p>
-      <p>{lorem(10)}?</p>
-      <OptionButton
-        selected={false}
-        text="Checkbox helper text"
-        onClick={() => undefined}
-        optionType="checkbox"
-        buttonType="primary"
-      />
-      <OptionButton
-        selected={true}
-        text="Checkbox helper text"
-        onClick={() => undefined}
-        optionType="checkbox"
-        buttonType="primary"
-      />
+      <p>Are you currently struggling with acne?</p>
+      <RadioButton
+        type="primary"
+        checked
+        css={css`
+          & > p:last-of-type {
+            margin-bottom: 0;
+          }
+        `}
+      >
+        Yes
+      </RadioButton>
+      <RadioButton
+        type="primary"
+        checked
+        css={css`
+          & > p:last-of-type {
+            margin-bottom: 0;
+          }
+        `}
+      >
+        No
+      </RadioButton>
       <p>Question 2:</p>
       <p>{lorem(10)}?</p>
       <p>Question 3:</p>

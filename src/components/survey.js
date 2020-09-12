ae54399c105c70e5564c3c4195f406969d9594b1
Sam <samuel_morgan_@outlook.com>
2020-09-12 12:20:02 +0100

    Added question 1B

--- src/components/survey.js
+++ src/components/survey.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react"
-import { ImmersiveModal, Button, OptionButton, RadioButton } from "radiance-ui"
+import { ImmersiveModal, Button, Dropdown, RadioButton } from "radiance-ui"
 import { lorem } from "../js/lorem"
 import { css } from "@emotion/core"
+import "../css/survey.css"
 
 const modalHeaderImage =
   "https://images.unsplash.com/photo-1556228852-80b6e5eeff06?ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80"
@@ -9,6 +10,7 @@ const modalHeaderImage =
 const Survey = props => {
   const [data, setData] = useState({
     acne: undefined,
+    length: undefined,
   })
   const { surveyOpen, setSurveyOpen } = props
 
@@ -26,7 +28,7 @@ const Survey = props => {
       <p>
         Thank you for taking our survey. It will take approximately 2-3 minutes.
       </p>
-      <p>Question 1:</p>
+      <p>Question 1{data.acne === true ? "A" : null}:</p>
       <p>Are you currently struggling with acne?</p>
       <RadioButton
         type="primary"
@@ -52,8 +54,58 @@ const Survey = props => {
       >
         No
       </RadioButton>
-      <p>Question 2:</p>
-      <p>{lorem(10)}?</p>
+      {data.acne === true && [
+        <p>Question 1B:</p>,
+        <p>How long have you had acne for?</p>,
+        <RadioButton
+          type="primary"
+          checked={data.length === 1}
+          css={css`
+            & > p:last-of-type {
+              margin-bottom: 0;
+            }
+          `}
+          onClick={() => setData({ ...data, length: 1 })}
+        >
+          Under 3 Months
+        </RadioButton>,
+        <RadioButton
+          type="primary"
+          checked={data.length === 2}
+          css={css`
+            & > p:last-of-type {
+              margin-bottom: 0;
+            }
+          `}
+          onClick={() => setData({ ...data, length: 2 })}
+        >
+          3 - 12 Months
+        </RadioButton>,
+        <RadioButton
+          type="primary"
+          checked={data.length === 3}
+          css={css`
+            & > p:last-of-type {
+              margin-bottom: 0;
+            }
+          `}
+          onClick={() => setData({ ...data, length: 3 })}
+        >
+          1 - 2 years
+        </RadioButton>,
+        <RadioButton
+          type="primary"
+          checked={data.length === 4}
+          css={css`
+            & > p:last-of-type {
+              margin-bottom: 0;
+            }
+          `}
+          onClick={() => setData({ ...data, length: 4 })}
+        >
+          2+ years
+        </RadioButton>,
+      ]}
       <p>Question 3:</p>
       <p>{lorem(10)}?</p>
       <p>Question 4:</p>
